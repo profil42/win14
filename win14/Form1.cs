ff1dbf1a69cf6171a4e6d487638bce3b0da2564a
profil42 <profil@live.de>
2017-12-13 08:36:43 +0100

    Hurra01

--- win14/Form1.cs
+++ win14/Form1.cs
@@ -16,5 +16,10 @@ namespace win14
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s = "";
+        }
     }
 }
